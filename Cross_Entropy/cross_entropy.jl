1ea3ef36ed74f85a1b08968c0b39cfc4009b57af
Ben-Rutkowski <70175023+Ben-Rutkowski@users.noreply.github.com>
2023-12-08 20:39:38 -0800

    before cleaned code

--- Cross_Entropy/cross_entropy.jl
+++ Cross_Entropy/cross_entropy.jl
@@ -57,7 +57,7 @@ function recalc(x_elite)
     return MultivariateNormal(mean, cov)
 end
 
-function initialSamples(D::DemandSystem, M, m, b, B_FIX=-1)
+function initialSamples(D::DemandSystem, M, m, B_FIX=-1)
     x_min, x_max = D.x_min, D.x_max
     y_min, y_max = D.y_min, D.y_max
     demand_max = D.demand_max
@@ -82,7 +82,7 @@ function initialSamples(D::DemandSystem, M, m, b, B_FIX=-1)
     if B_FIX == -1
         b_samples = float(rand(1:M, 1, m))
     else
-        b_samples = b*ones(1, m)
+        b_samples = B_FIX*ones(1, m)
     end
 
     # x_sample = vcat(x_sample, b*ones(1, m))
@@ -94,7 +94,7 @@ end
 
 # ================ Cross Entropy Methods ================
 function crossEntropyLegalCorrection(D::DemandSystem, func, M, k_max, B_FIX=-1, m=100, m_elite=10)
-    x_sample = initialSamples(D, M, m, M, B_FIX)
+    x_sample = initialSamples(D, M, m, B_FIX)
     order    = sortperm( [func(x_sample[:,i]) for i in 1:m] )
     x_legal  = first_mLegal(D, x_sample, order, m_elite)
     P = recalc(x_legal)
@@ -112,19 +112,20 @@ function crossEntropyLegalCorrection(D::DemandSystem, func, M, k_max, B_FIX=-1, 
     order    = sortperm( [func(x_sample[:,i]) for i in 1:m] )
     x_legal  = first_mLegal(D, x_sample, order, m_elite)
     
-    return x_legal[:,1], pushToFeasible(D, x_legal[:,1])
+    # return x_legal[:,1], pushToFeasible(D, x_legal[:,1])
+    return x_legal[:,1]
 end
 
 
 function crossEntropy(D::DemandSystem, func, M, k_max, B_FIX=-1, m=100, m_elite=10)
-    x_sample = initialSamples(D, M, m, M, B_FIX)
+    x_sample = initialSamples(D, M, m, B_FIX)
     order    = sortperm( [func(x_sample[:,i]) for i in 1:m] )
     x_elite  = first_mLegal(D, x_sample, order, m_elite)
     P = recalc(x_elite)
 
     for k = 1:k_max
         x_sample = correctMatrix(rand(P, m), B_FIX)
-        order   = sortperm( [func(x_sample[:,i]) for i in 1:m] )
+        order    = sortperm( [func(x_sample[:,i]) for i in 1:m] )
 
         x_elite = x_sample[:,order[1:m_elite]]        
         x_elite = pushToFeasibleMatrix(D, x_elite)
@@ -135,7 +136,8 @@ function crossEntropy(D::DemandSystem, func, M, k_max, B_FIX=-1, m=100, m_elite=
     order    = sortperm( [func(x_sample[:,i]) for i in 1:m] )
     x_elite  = x_sample[:,order[1:m_elite]]        
     
-    return x_elite[:,1], pushToFeasible(D, x_elite[:,1])
+    # return x_elite[:,1], pushToFeasible(D, x_elite[:,1])
+    return x_elite[:,1]
 end
 
 
